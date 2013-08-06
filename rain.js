46c3d3784091625b8d268343c79aff8f012f555f
Chris Sammis <csammis@gmail.com>
2013-08-05 20:46:50 -0500

    Using a set implementation to run multiple raindrops

--- rain.js
+++ rain.js
@@ -5,6 +5,49 @@
     var HEIGHT = canvas.height;
     var WIDTH = canvas.width;
     cxt.fillStyle = "#ffffff";
+
+    // Set implementation from http://matt.stumpnet.net/
+    function Set(name) {
+        var setName = name, index = 0, count = 0;
+
+        this.size = function() {
+            return count;
+        };
+
+        this.add = function(item) {
+            if (this.contains(item)) {
+                return;
+            }
+            item[setName] = index;
+            this[index] = item;
+            index++;
+            count++;
+        };
+
+        this.remove = function(item) {
+            if (!this.contains(item)) {
+                return;
+            }
+            delete this[item[setName]];
+            delete item[setName];
+            count--;
+        };
+
+        this.clear = function() {
+            for (var p in this) {
+                if (this.contains(this[p])) {
+                    this.remove(this[p]);
+                }
+                index = 0;
+            }
+        };
+
+        this.contains = function(item) {
+            return item.hasOwnProperty(setName)
+                    && this.hasOwnProperty(item[setName])
+                    && item === this[item[setName]];
+        };
+    };
     
     function raindrop(init_x, init_y, init_wind, init_length) {
         var x = init_x, y = init_y, wind = init_wind, length = init_length;
@@ -22,19 +65,31 @@
         };
 
         this.isDone = function() {
-            return y > HEIGHT + 1;
+            return y > HEIGHT + 1 || x > WIDTH + 1;
         };
     };
 
-    var r = new raindrop(100, 0, 1, 10);
+    var raindrops = new Set("raindrops");
 
     function timer() {
         var t = setTimeout(function () { timer(); }, 2);
+        
+        while (raindrops.size() < 10) {
+            var x = Math.floor(Math.random() * WIDTH);
+            raindrops.add(new raindrop(x, 0, 1, 10));
+        }
+
         cxt.fillRect(0, 0, canvas.width, canvas.height);
-        r.update();
-        r.draw();
-        if (r.isDone()) {
-            clearTimeout(t);
+        for (var r in raindrops)
+        {
+            if (raindrops.hasOwnProperty(r) && raindrops.contains(raindrops[r]))
+            {
+                raindrops[r].update();
+                raindrops[r].draw();
+                if (raindrops[r].isDone()) {
+                    raindrops.remove(raindrops[r]);
+                }
+            }
         }
     };
 
